811b6d1dbaefc910ad516473cb89589321f4aa4b
Raul Lourenco <raulourenco@hotmail.com>
2023-08-14 14:53:39 -0300

    refactor: removendo post para insercao de pedido + ajustes finos

--- src/main/java/br/com/orders/controller/OrdersController.java
+++ src/main/java/br/com/orders/controller/OrdersController.java
@@ -7,7 +7,6 @@ import br.com.orders.dto.TotalQuantityByClientResponse;
 import br.com.orders.dto.TotalValueResponse;
 import br.com.orders.mapper.ClientMapper;
 import br.com.orders.mapper.OrderMapper;
-import br.com.orders.service.ClientService;
 import br.com.orders.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,37 +25,20 @@ public class OrdersController {
     private OrderService orderService;
 
     @Autowired
-    private ClientService clientService;
-
-    @Autowired
     private OrderMapper orderMapper;
 
     @Autowired
     private ClientMapper clientMapper;
 
-    private static Logger log = LoggerFactory.getLogger(OrdersController.class);
-
-    @PostMapping("/saveOrder")
-    public Order saveOrder(@RequestBody Order order) {
-        log.info("Criando pedido na base de dados. Pedido={}", order);
-
-        Client client = clientMapper.mapFrom(order);
-        clientService.persistClient(client);
-
-        Order persisted = orderService.persistOrder(order);
-
-        log.info("Pedido persistido na base de dados. Pedido={}", persisted);
-
-        return persisted;
-    }
+    private static final Logger log = LoggerFactory.getLogger(OrdersController.class);
 
     @GetMapping("/total_value/{orderId}")
     public ResponseEntity<TotalValueResponse> getTotalValue(@PathVariable final int orderId) {
         log.info("Buscando pedido na base de dados. ID do Pedido={}", orderId);
 
-        Order order = orderService.findById(orderId);
+        final Order order = orderService.findById(orderId);
 
-        TotalValueResponse response = orderMapper.mapFromOrder(order);
+        final TotalValueResponse response = orderMapper.mapFromOrder(order);
 
         log.info("Response de valor total retornado com sucesso. Response={}", response);
 
@@ -67,9 +49,9 @@ public class OrdersController {
     public ResponseEntity<TotalQuantityByClientResponse> getTotalQuantity(@PathVariable final int clientId) {
         log.info("Buscando quantidade de pedidos por cliente. ID do Cliente={}", clientId);
 
-        Client orderQuantityByClient = orderService.findOrderQuantityByClient(clientId);
+        final Client orderQuantityByClient = orderService.findOrderQuantityByClient(clientId);
 
-        TotalQuantityByClientResponse response = clientMapper.mapToResponse(orderQuantityByClient);
+        final TotalQuantityByClientResponse response = clientMapper.mapToResponse(orderQuantityByClient);
 
         log.info("Response de quantidade total retornado com sucesso. Response={}", response);
 
@@ -80,9 +62,9 @@ public class OrdersController {
     public ResponseEntity<OrderListByClientResponse> getOrdersList(@PathVariable final int clientId) {
         log.info("Buscando lista de pedidos por cliente. ID do Cliente={}", clientId);
 
-        List<Order> allOrdersByClient = orderService.findAllOrdersByClient(clientId);
+        final List<Order> allOrdersByClient = orderService.findAllOrdersByClient(clientId);
 
-        OrderListByClientResponse response = getResponse(allOrdersByClient);
+        final OrderListByClientResponse response = getResponse(allOrdersByClient);
 
         log.info("Response de lista de pedidos retornado com sucesso. Response={}", response);
 
